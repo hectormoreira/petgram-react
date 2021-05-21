aa6391707f826f8c2cb30870cd74b0ac686dba0f
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2021-05-20 23:22:19 -0500

    custom hooks, localstorage

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,30 +1,21 @@
 import React, { useEffect, useRef, useState } from "react";
 import { ImgWrapper, Img, Button, Article } from "./styles";
-import { MdFavoriteBorder } from "react-icons/md";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { useNearScreen } from "../../hooks/useNearScreen";
+
 
 const DEFAULT_IMAGE =
   "https://images.unsplash.com/photo-1531523668299-e20047c89111?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const ref = useRef(null);
-  const [show, setShow] = useState(false);
 
-  useEffect(() => {
-    Promise.resolve(
-      typeof window.IntersectionObserver !== "undefined"
-        ? window.IntersectionObserver
-        : import("intersection-observer")
-    ).then(() => {
-      const observer = new window.IntersectionObserver((entries) => {
-        const { isIntersecting } = entries[0];
-        if (isIntersecting) {
-          setShow(true);
-          observer.disconnect();
-        }
-      });
-      observer.observe(ref.current);
-    });
-  }, [ref]);
+  const [show, ref] = useNearScreen();
+
+  const key = `like-${id}`;
+  const [liked, setLiked] = useLocalStorage(key, false);
+
+  const Icon = liked ? MdFavorite : MdFavoriteBorder;
 
   return (
     <Article ref={ref}>
@@ -36,8 +27,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             </ImgWrapper>
           </a>
 
-          <Button>
-            <MdFavoriteBorder size="32px" /> {likes} likes!
+          <Button onClick={() => setLiked(!liked)}>
+            <Icon size="32px" /> {likes} likes!
           </Button>
         </>
       )}
