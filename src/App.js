033c5a1161d8bf0cdce98fe36a8d84808906bb7b
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2021-05-26 19:23:38 -0500

    render props

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React from "react";
 import { ListOfCategories } from "./components/ListOfCategories";
 import { ListOfPhotoCards } from "./components/ListOfPhotoCard";
 import { Logo } from "./components/Logo";
+import { PhotoCardWithQuery } from "./container/PhotoCardWithQuery";
 import { GlobalStyle } from "./styles/GlobalStyles";
 
-const App = () => (
+const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search);
+  const detailId = urlParams.get("detail");
+
+  return (
     <>
-    <GlobalStyle/>
-    <Logo/>
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={2}/>
+      <GlobalStyle />
+      <Logo />
+      {detailId ? (
+        <PhotoCardWithQuery id={detailId}/>
+      ) : (
+        <>
+          <ListOfCategories />
+          <ListOfPhotoCards categoryId={2} />
+        </>
+      )}
     </>
-);
+  );
+};
 
 export default App;
